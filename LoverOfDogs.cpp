913cde9d5f1b38c2ab95d3c9c3bc8f1fe4f21a1a
Parakram Basnet <parakram.basnet@gmail.com>
2017-12-01 10:48:30 -0500

    Update LoverOfDogs.cpp
    Added lower case letter checking on name and color and added a few extra methods into the class Pet named colorDecision() and ageDecision()

--- LoverOfDogs.cpp
+++ LoverOfDogs.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <iostream>		 // std::cout and std::cin
+#include <string>		// std::string
+#include <locale>	   // std::locale and std::tolower
 
 using namespace std;
 
@@ -6,16 +8,18 @@ class Pet{
 	
 	public: 
 	
-	string pet;
+	string petVar;
+	string colorVar;
+	int ageVar;
 	
 	bool checkPet(){
 		
-		if (pet == "cat"){
+		if (petVar == "cat"){
 			
 			return false;
 		}
 		
-		else if (pet == "dog"){
+		else if (petVar == "dog"){
 			
 			return true;
 		}
@@ -25,19 +29,67 @@ class Pet{
 			return false;
 		}
 	}
+	
+	
+	void colorDecision(){
+		
+		if (colorVar == "black"){
+			
+			cout << "\nOH! a stealthy little doggo. I love it!";
+		}
+		
+		else if (colorVar == "white"){
+			
+			cout << "\nFluffy and adorable little white pupper!";
+		}
+		
+		else{
+			
+			cout << "\nPaint it black or white bruv.";
+		}
+	}
+	
+	bool ageDecision(){
+		
+		if (ageVar < 10){
+			
+			return true;
+		} 
+		
+		else if (ageVar > 10){
+			
+			return false;
+		}
+		
+		else{
+			
+			return false;
+		}
+	}
 };
 
+	
 
 int main(){
+	
+	locale loc;
 	
 	cout << "What kind of pet do you have: ";
 	string pet;
 	cin >> pet;
 	
-	Pet newPet;
-	newPet.pet = pet;
+	string petNew;  // setting a new variable to store our lower case string 
 	
-	bool decision = newPet.checkPet();
+	for (int i = 0; i < pet.length(); i++){
+		
+		petNew += tolower(pet[i],loc);
+	}
+	
+	
+	Pet newPet;   // Creating an object for class Pet. Object name = newPet
+	newPet.petVar = petNew;   // initializing the public instance variable petVar from the Pet class
+	
+	bool decision = newPet.checkPet();   // Calling the checkPet method from the Pet class using the newPet object and storing the return value in variable "decision"
 	
 	if (decision == true){
 		
@@ -46,16 +98,47 @@ int main(){
 		cout << "\n\nWhat color is your pet: ";
 		string color;
 		cin >> color;
+		
+		string newColor;
+		
+		for (int i = 0; i < color.length(); i++){
+			
+			newColor += tolower(color[i], loc);
+		}
+		
+		color = newColor;
+		
 	
 		cout << "What is the age of your " << pet << ":";
 		int age;
 		cin >> age;
+		
+		newPet.colorVar = color;  // public instance variable initiaization
+		newPet.colorDecision();   // Using class colorDecision
+		
+		cout << endl;
+		
+		newPet.ageVar = age;
+		bool ageFinal = newPet.ageDecision();
+		
+		if (ageFinal == true){
+			
+			cout << "But the pupper has a lot to learn!";
+		}
+		
+		else{
+			
+			cout << "And the doggo has seen a lot!";
+		}
+		
+		cout << endl;
+		
 	
 	}
 	
 	else{
 		
-		cout << "Sorry, a " << pet << " is ineligible to be housed in our property.";
+		cout << "\nSorry, a " << pet << " is ineligible to be housed in our property.";
 	}
 	
 	return 0;
